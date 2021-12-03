9d624dbade6b952769002ed6a73a5b695d269a16
dzinevich <darjakisel@gmail.com>
2021-12-03 13:45:54 +0200

    Secure config and eureka with basic auth

--- config/build.gradle.kts
+++ config/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.cloud:spring-cloud-config-server")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ discovery/src/main/kotlin/com/dzinevich/discovery/config/SecurityConfig.kt
@@ -0,0 +1,19 @@
+package com.dzinevich.discovery.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http.csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+}
+
+
